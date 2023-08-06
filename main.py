761ae25160dfdf1eb9a7b6375e0b39a9bfa1214d
Guilherme Casagrande <thegui4000@gmail.com>
2023-08-06 02:21:14 -0300

    finalizado?

--- main.py
+++ main.py
@@ -1,29 +1,62 @@
 from __future__ import print_function
-
 import os.path
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 import googleapiclient.discovery 
 from googleapiclient.errors import HttpError
-
+import csv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/contacts', 'https://www.googleapis.com/auth/contacts.readonly']
 
+def pathCsv():
+    janela_padrao = Tk().withdraw()
+    caminho_do_arquivo = askopenfilename(filetypes = (("Arquivos de texto", "*.csv"), ("Arquivos csv", "*.csv")))
 
-def main():
-    """Shows basic usage of the People API.
-    Prints the name of the first 10 connections.
-    """
+    if caminho_do_arquivo:
+        with open(caminho_do_arquivo, encoding='latin_1') as arquivo:
+            return arquivo
+    else:
+        return None
+
+def salvarContato(firstName, phone, creds, email = ''):
+    
+
+    try:
+        # Get the Google API client
+        client = googleapiclient.discovery.build('people', 'v1', credentials=creds)
+
+        contact = {
+            'names': [{'givenName': firstName}],
+            'phoneNumbers': [{'value': phone}],
+            'emailAddresses':[{'value': email}]
+        }
+
+        # Call the People API to create the contact
+        response = client.people().createContact(body=contact).execute()
+
+        # Print the contact's ID
+        print(f'Contato de {name} salvo!')
+    except HttpError as err:
+        print(err)
+
+def csv_to_dict(csv_file_path):
+    data_dict = []
+    with open(csv_file_path, 'r', newline='', encoding="UTF-8") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            data_dict.append(row)
+    return data_dict
+
+
+if __name__ == '__main__':
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -35,33 +68,15 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-    try:
-        # Get the Google API client
-        client = googleapiclient.discovery.build('people', 'v1', credentials=creds)
+    path = pathCsv()
+    if path == None:
+        print('Arquivo não encontrado!')
+        exit()
+    peoples = csv_to_dict(path.name)
 
-        # Set the contact's name
-        name = 'Guilherme Casagrande'
-
-        # Set the contact's email address
-        email = 'gui@casagrande.com'
-
-        # Set the contact's phone number
-        phone = '+55 11 9999-9999'
-
-        # Create the contact
-        contact = {
-    'names': [{'givenName': 'Guilherme', 'familyName': 'Casagrande'}],
-    'phoneNumbers': [{'value': phone}]
-}
-
-        # Call the People API to create the contact
-        response = client.people().createContact(body=contact).execute()
-
-        # Print the contact's ID
-        print(response['resourceName'])
-    except HttpError as err:
-        print(err)
-
-
-if __name__ == '__main__':
-    main()
+    for people in peoples:
+        name = people['nome']
+        email = people['email']
+        phone = people['celular']
+        phone = f'+55 {phone}'
+        salvarContato(name, phone, creds, email)
